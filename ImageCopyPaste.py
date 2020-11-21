494fac61419141aa2eadbb4c90a33dc2cdd401aa
kdaruwalla <74469956+kdaruwalla@users.noreply.github.com>
2020-11-21 15:20:28 -0500

    Add files via upload

---
+++ ImageCopyPaste.py
@@ -0,0 +1,14 @@
+from PIL import Image
+
+
+#function that copies one of the stream captures and pastes it over the other
+def imageCopy(img, img2):
+    #img = Image.open(r'C:\Users\Kivan\OneDrive\Pictures\Camera Roll\image2.jpg', 'r') #represents the image to be pasted
+    #img2 = Image.open(r'C:\Users\Kivan\OneDrive\Pictures\Camera Roll\image1.jpg', 'r')#represnets the image to get pasted over
+    img_w, img_h = img.size
+    #background = Image.new('RGBA', (1440, 900), (255, 255, 255, 255))
+    background = Image.new('RGBA', (2560, 1600), (255, 255, 255, 255))
+    bg_w, bg_h = background.size
+    offset = ((bg_w - img_w) // 2, (bg_h - img_h) // 2)
+    img2.paste(img, offset)
+    img2.save(r'C:\Users\Kivan\OneDrive\Pictures\Camera Roll\image1.jpg')
